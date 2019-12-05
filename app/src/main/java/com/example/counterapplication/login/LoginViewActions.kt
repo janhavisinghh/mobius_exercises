f433b57566ded66929c892e4fac279d38c41474a
janhavisingh <janhavi.shanu@gmail.com>
2019-12-05 14:08:16 +0530

    Login View Actions

---
+++ app/src/main/java/com/example/counterapplication/login/LoginViewActions.kt
@@ -0,0 +1,11 @@
+package com.example.counterapplication.login
+
+interface LoginViewActions {
+
+    fun showErrorMessageToast(error: NetworkErrorMessage)
+    fun clearUsernameError()
+    fun clearPasswordError()
+    fun showHomeScreen()
+    fun saveToken(token: String)
+
+}
